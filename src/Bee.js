b747d8fd41b8f7a305c69ad195d8fb8b545456e4
Will Wheeler <willfulbard@gmail.com>
2016-03-07 11:22:32 -0800

    Add prop job and set to 'keep on growing'

--- src/Bee.js
+++ src/Bee.js
@@ -2,6 +2,7 @@ var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 Bee.prototype = Object.create(Grub.prototype);
 Bee.constructor = Grub;
