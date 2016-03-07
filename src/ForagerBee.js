67ddff4c09fb1659005034772ae56cb87b828990
Will Wheeler <willfulbard@gmail.com>
2016-03-07 11:37:08 -0800

    Add canFly and treasureChest props

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -2,6 +2,8 @@ var ForagerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Bee.prototype);
